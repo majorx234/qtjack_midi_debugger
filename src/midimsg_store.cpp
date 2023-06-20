400cafcd77ac1b5d83ee37f90f8bead99aa3bc78
Majorx234 <majorx234@googlemail.com>
2023-06-20 02:41:10 +0200

    Add sorting of recorded MidiMsgs

--- src/midimsg_store.cpp
+++ src/midimsg_store.cpp
@@ -1,8 +1,20 @@
+#include <algorithm>
 #include "midimsg_store.hpp"
 #include <midimsg_store.hpp>
 
 MidiMsgStore::MidiMsgStore() {}
 MidiMsgStore::~MidiMsgStore() {}
-void MidiMsgStore::add_midi_msg(QtJack::MidiMsg){}
-void MidiMsgStore::reset() {}
-void MidiMsgStore::save_as_midi_file(std::string path) {}
+void MidiMsgStore::add_midi_msg(QtJack::MidiMsg msg){
+  midi_msg_list << msg;
+}
+void MidiMsgStore::reset() {
+  midi_msg_list.clear();
+}
+void MidiMsgStore::save_as_midi_file(std::string path) {
+  // sort after timestamp
+  std::sort (midi_msg_list.begin(),
+             midi_msg_list.end(),
+             [](QtJack::MidiMsg &msg1,QtJack::MidiMsg &msg2){return (msg1.timestamp < msg2.timestamp);} );
+  Qtjack::MidiMsg first = midi_msg_list.first();
+  // ToDo: fix time stamps of midievents in new midifile
+}
