6d64c9109116bf66519859e962409d8b22371817
Majorx234 <majorx234@googlemail.com>
2023-06-21 23:40:18 +0200

    Fix wrong type definition

--- src/midimsg_store.cpp
+++ src/midimsg_store.cpp
@@ -7,14 +7,16 @@ MidiMsgStore::~MidiMsgStore() {}
 void MidiMsgStore::add_midi_msg(QtJack::MidiMsg msg){
   midi_msg_list << msg;
 }
+
 void MidiMsgStore::reset() {
   midi_msg_list.clear();
 }
+
 void MidiMsgStore::save_as_midi_file(std::string path) {
   // sort after timestamp
   std::sort (midi_msg_list.begin(),
              midi_msg_list.end(),
              [](QtJack::MidiMsg &msg1,QtJack::MidiMsg &msg2){return (msg1.timestamp < msg2.timestamp);} );
-  Qtjack::MidiMsg first = midi_msg_list.first();
+  QtJack::MidiMsg first = midi_msg_list.first();
   // ToDo: fix time stamps of midievents in new midifile
 }
